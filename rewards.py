a16d43891d14d3def244c5bbcaff6c5db4f8f811
1ceseismic <102632048+1ceseismic@users.noreply.github.com>
2026-02-28 17:45:30 +1300

    basic rewards

---
+++ rewards.py
@@ -0,0 +1,35 @@
+import numpy as np
+from typing import Any, Dict, List, Tuple
+
+from rlgym.api import AgentID, RewardFunction
+from rlgym.rocket_league.api import GameState
+from rlgym.rocket_league.common_values import CAR_MAX_SPEED
+from rlgym.rocket_league.obs_builders import DefaultObs
+
+class VelocityPlayerToBallReward(RewardFunction[AgentID, GameState, float]):
+    def reset(
+        self,
+        agents: List[AgentID],
+        initial_state: GameState,
+        shared_info: Dict[str, Any],
+    ) -> None:
+        pass
+
+    def get_rewards(
+        self,
+        agents: List[AgentID],
+        state: GameState,
+        is_terminated: Dict[AgentID, bool],
+        is_truncated: Dict[AgentID, bool],
+        shared_info: Dict[str, Any],
+    ) -> Dict[AgentID, float]:
+        return {agent: self._get_reward(agent, state) for agent in agents}
+
+    def _get_reward(self, agent: AgentID, state: GameState):
+        ball = state.ball
+        car = state.cars[agent].physics
+
+        car_to_ball = ball.position - car.position
+        car_to_ball = car_to_ball / np.linalg.norm(car_to_ball)
+
+        return np.dot(car_to_ball, car.linear_velocity) / CAR_MAX_SPEED
